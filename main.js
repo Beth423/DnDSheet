b024212f9a7f5dfe8ebe77874e233a82ef07c95b
Beth423 <59936151+Beth423@users.noreply.github.com>
2024-05-13 00:45:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-mainSec=document.getElementById("mainSec");
-mainSec.addEventListener('load',main);
-
+let mainSec=document.getElementById("mainSec");
+mainSec.addEventListener("load",main());
 
 function main (){
-    console.log("here");
+alert("here");
 };
+//  todo add action nav as action switch case statment 
+function impText(){
+    // makes text box imput name Spell manes and other possablae imputs 
+}
+function impNum(){
+    //TODO makes the number imput up to 20
+}
+// todo make cash var and hp 
+// todo make  make the calculater function for user stats 
