a4f99685b597a83e90d45b7508726b4f527636d8
Beth423 <59936151+Beth423@users.noreply.github.com>
2024-05-13 00:45:13 -0700

    Update forms.js

---
+++ components/forms.js
@@ -0,0 +1,7 @@
+let mainsec = document.getElementById("mainSec")
+
+function addImputNumber (){
+    console("add number ");
+    let imput = document.createElement("input");
+    imput.setAttribute("type","number");
+}
