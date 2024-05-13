22247f26373d96928494b15095f2322f50b49cc8
Beth423 <59936151+Beth423@users.noreply.github.com>
2024-05-13 15:40:42 -0500

    Delete forms.js

--- components/forms.js
+++
@@ -1,7 +0,0 @@
-let mainsec = document.getElementById("mainSec")
-
-function addImputNumber (){
-    console("add number ");
-    let imput = document.createElement("input");
-    imput.setAttribute("type","number");
-}
