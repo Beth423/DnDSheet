3a5500216fdf9b4cf69b33f6ff7932997ddb8510
Beth423 <59936151+Beth423@users.noreply.github.com>
2024-05-15 13:56:38 -0700

    Update comonents.js

--- comonents.js
+++ comonents.js
@@ -3,4 +3,23 @@ function nav(){
     a.innerText=("text");
     // a.innerHTML("src","#");
     console.log(a);
+    creatForm();
+}
+function creatForm(){
+    console.log("add form");
+   let form= document.createElement("form");
+    formText("number","int","int");
+    append(form);
+
+}
+function formText(name){
+    let text= document.createElement("imput");
+    text.setAttribute("value",name)
+    
+};
+function append (x){
+    main.appendChild(x);
+}
+function appendInside(x,y){
+    x.appendChild(y);
 }
