af08cedc57a80e79c45d6ff848a98868e4e9d9ce
Beth423 <59936151+Beth423@users.noreply.github.com>
2024-05-19 02:07:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ let main = document.getElementById("main");
 main.addEventListener('load',onload());
 let page ="home";
 function onload(){
-    //TODO add a nav section
-creatForm();
+    console.log("here on load")
+    creatForm();
 }
