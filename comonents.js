3797eb41ffb69d8c698f0c13afe26fb7e60d95e9
Beth423 <59936151+Beth423@users.noreply.github.com>
2024-05-21 00:22:54 -0700

    Update comonents.js

--- comonents.js
+++ comonents.js
@@ -6,18 +6,42 @@ function nav(){
 }
 function creatForm(){
    let form= document.createElement("form");
-   form.setAttribute("action","home");
+   let stats=["str","int","con","dex","wiz"]
+   form.setAttribute("action","form.php");
     append(form);
-    formImput("number","str",0);
+    console.log("creat form");
+    // formImput("number","str",0);
     //TODO than exsport the form var to the other function to sind it to 
-    function formImput(type,name,value){
+    for(let i=0 ;i<=4;i++){
+        // console.log(stats[i])
+        let label = document.createElement("lable");
+        //testing label in the consale first befor sinding to dom 
+        label.setAttribute("class","label");
+        label.innerText=(stats[i]);
+        // using rado button 
+        //? look up whether a function var can be sent back to the calling function
+        let rado= document.createElement("imput");
+        rado.setAttribute("type","rado");
+        rado.setAttribute("name","rado");
+        console.log(rado)
         let text= document.createElement("input");
-        text.setAttribute("type",type);
-        text.setAttribute("name",name);
-        text.setAttribute("value",value);
-        form.appendChild(text)
+        text.setAttribute("type","number");
+        text.setAttribute("name",stats[i]);
+        text.setAttribute("placeHolder",0 );
+        text.setAttribute("class","numberImput");
+        form.appendChild(label);
+        form.appendChild(rado);
+        form.appendChild(text);
     };
 }
+// function formImput(type,name,value){
+//     let text= document.createElement("input");
+//     text.setAttribute("type",type);
+//     text.setAttribute("name",name);
+//     text.setAttribute("value",value);
+//     form.appendChild(text)
+//     console.log(form)
+// };
 
 function append (x){
     main.appendChild(x);
