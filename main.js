3602d57501a7eb0d1ef9a1bf52e89513b3cec091
Beth423 <59936151+Beth423@users.noreply.github.com>
2024-05-21 12:28:11 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ let main = document.getElementById("main");
 main.addEventListener('load',onload());
 let page ="home";
 function onload(){
-    console.log("here on load")
+    console.log("here on load");
     creatForm();
 }
