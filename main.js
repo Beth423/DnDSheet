bdf6edaa3910b26a439c2a79dde83aa6ffb83538
Beth423 <59936151+Beth423@users.noreply.github.com>
2024-05-29 23:48:39 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 let main = document.getElementById("main");
 main.addEventListener('load',onload());
 let page ="home";
+
 function onload(){
     console.log("here on load");
     creatForm();
 }
+//todo  switch case based on the page varrun functions acordingly
